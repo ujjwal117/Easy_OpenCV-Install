0b936f4ef3aa28d77a4932a6a11c6aaef6a20c6a
Ujjwal Biswas <ujjwal117@users.noreply.github.com>
2018-05-01 23:45:52 +0530

    Update openCV_installation.sh

--- openCV_installation.sh
+++ openCV_installation.sh
@@ -12,19 +12,20 @@ echo "##########################################################################
                                                             By Ujjwal Biswas
                                                                 V- 1.0
 ###############################################################################################################################"
-sudo apt install update
-sudo apt upgrade
+sudo apt update
+sudo apt upgrade -y
 #################################################################################################
 #cd ~/
 echo "#############################################################################################################################
                                                  CREATING NEW DIRECTORY
 ##############################################################################################################################"
 sudo mkdir OpenCV_3
+pwd
 cd OpenCV_3
 echo "###############################################################################################################################
                                                   GETTING GIT
 ################################################################################################################################"
-sudo apt install git
+sudo apt install -y git
 echo "Cloning OpenCV sourc from github to opencv-3.3.1"
 sudo git clone https://github.com/opencv/opencv.git
 echo "Cloning opencv-contrib from github to opencv-contrib"
@@ -34,41 +35,40 @@ sudo git clone https://github.com/opencv/opencv_contrib.git
 echo  "#################################################################################################################################
                               INSTALLING THE DEPENDENCY FOR BUILDING OPENCV FROM THE SOURCES
 #################################################################################################################################"
-sudo apt install build-essential cmake  libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-
+sudo apt install -y build-essential cmake  libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev 
 python3.6-config --includes
-sudo apt-get install libjpeg-dev libtiff5-dev  libpng12-dev
+sudo apt-get install -y libjpeg-dev libtiff5-dev  libpng12-dev
 
 echo "#################################################################################################################################
                                                       ADDING REPOSITORY FOR libjasper-dev
 #################################################################################################################################"
 sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
 sudo apt update
-sudo apt install libjasper1 libjasper-dev
+sudo apt install -y libjasper1 libjasper-dev
 echo "#################################################################################################################################
                                                                   INSTALLING GTK3.0
 #################################################################################################################################"
-sudo apt-get install libgtk-3-dev
+sudo apt-get install -y libgtk-3-dev
 
 echo "#################################################################################################################################
                                                        INSTALLING PYTHON3.6 DEV and NUMPY
 #############################################################################################################################"
-sudo apt-get install python3.6-dev python3.6-numpy libtbb2 libtbb-dev libdc1394-22-dev
+sudo apt-get install -y python3.6-dev python3.6-numpy libtbb2 libtbb-dev libdc1394-22-dev
 
 
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 
-sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
 echo "#############################################################################################################################
                                               CHANGING DIRECTORY AND CREATING BUILD DIRECTORY
 #############################################################################################################################"
-cd opencv-3.3.1
+cd opencv
 sudo mkdir build
 cd build
 echo "#############################################################################################################################
                                                           CMAKE PROCESS *(HAVE A CUP OF COFFEE)
 #############################################################################################################################"
-sudo cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D OPENCV_EXTRA_MODULES_PATH=~/OpenCV_3/opencv_contrib-3.3.1/modules ..
+sudo cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D OPENCV_EXTRA_MODULES_PATH=~/OpenCV_3/opencv_contrib/modules ..
 
 sudo make clean
 echo "#############################################################################################################################
